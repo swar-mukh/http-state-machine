e09bff84bb81a2dbb4481569a44d8c9d76366d42
Swarnava Mukherjee <swar_mukh10@yahoo.co.in>
2021-08-06 12:13:43 +0530

    Update internal working mechanism of StateMachine to work with new ClientState API

--- src/lib/StateMachine.js
+++ src/lib/StateMachine.js
@@ -1,45 +1,58 @@
-function StateMachine(initialState) {
-    this.currentState = initialState
+const defaultStateMachineConfiguration = {
+    suppressHateoas: false,
+    hateoasAttributeName: "hateoas"
+}
+
+function StateMachine(configuration = defaultStateMachineConfiguration) {
+    this.configuration = configuration
 }
 
 StateMachine.prototype = {
-    next: function (event, payload) {
-        let response = {}
+    next: function ({ request, response }, client) {
+        if (client.currentState.getApplicableEvents().includes(request.url.pathname)) {
 
-        if (this.currentState.getApplicableEvents().includes(event)) {
+            client.currentState.onExit()
 
-            this.currentState.onExit()
-
-            const transitionObject = this.currentState.events.get(event)
+            const transitionObject = client.currentState.events.get(request.url.pathname)
 
             switch (transitionObject.type) {
                 case "normal": {
-                    transitionObject.action(payload)
-                    this.currentState = transitionObject.transitionTo
+                    transitionObject.action(request, response)
+                    client.currentState = transitionObject.transitionTo
                     break
                 }
                 case "self": {
-                    transitionObject.action(payload)
+                    transitionObject.action(request, response)
                     break
                 }
                 case "boolean": {
-                    if (transitionObject.action(payload)) {
-                        this.currentState = transitionObject.transitionToOnSuccess
+                    if (transitionObject.action(request, response)) {
+                        client.currentState = transitionObject.transitionToOnSuccess
                     } else {
-                        this.currentState = transitionObject.transitionToOnFailure
+                        client.currentState = transitionObject.transitionToOnFailure
                     }
                     break
                 }
                 default: { }
             }
 
-            this.currentState.onEntry()
-            response = this.currentState.invoke()
+            client.currentState.onEntry()
+            client.currentState.invoke()
+
+            if (!this.configuration.suppressHateoas) {
+                response.write(JSON.stringify({ hateoas: client.currentState.getApplicableEvents() }))
+            }
         } else {
-            response = { message: "Invalid request!" }
+            response.writeHead(404, {
+                "Content-Type": "application/json"
+            })
+            response.write(JSON.stringify({
+                message: "Invalid Path!",
+                hateoas: client.currentState.getApplicableEvents()
+            }))
         }
 
-        return response
+        response.end()
     }
 }
 
