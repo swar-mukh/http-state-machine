cea2e89c130595dbe5a14d7846dbe9b35fcd8eaf
Swarnava Mukherjee <swar_mukh10@yahoo.co.in>
2021-08-06 12:21:10 +0530

    Expose new APIs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const StateMachine = require("./lib/StateMachine.js")
 const State = require("./lib/State.js")
 const Transitions = require("./lib/Transitions.js")
+const ClientState = require("./lib/ClientState.js")
+
+const HttpIO = require("./util/HttpIO")
+
+const util = {
+    simplifyRequest: HttpIO.simplifyRequest
+}
 
 module.exports = {
     StateMachine,
     State,
-    Transitions
+    Transitions,
+    ClientState,
+    util
 }
