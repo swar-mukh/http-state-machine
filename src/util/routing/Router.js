c2539a87275dbbb85be1b83acb0741e3d4dbbe6b
Swarnava Mukherjee <swar_mukh10@yahoo.co.in>
2021-08-07 18:45:32 +0530

    Add various methods to Router

--- src/util/routing/Router.js
+++ src/util/routing/Router.js
@@ -1,8 +1,15 @@
 const Route = require("./Route")
 
 const Router = {
+    // connect: path => new Route("CONNECT", path),
+    delete: path => new Route("DELETE", path),
     get: path => new Route("GET", path),
-    post: path => new Route("POST", path)
+    head: path => new Route("HEAD", path),
+    // options: path => new Route("OPTIONS", path),
+    patch: path => new Route("PATCH", path),
+    post: path => new Route("POST", path),
+    put: path => new Route("PUT", path),
+    // trace: path => new Route("TRACE", path),
 }
 
 module.exports = Router
