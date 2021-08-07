b9ac3b9269211ba9d66e4d145e081dcd5df3f1fa
Swarnava Mukherjee <swar_mukh10@yahoo.co.in>
2021-08-07 18:31:02 +0530

    Refactor demo example

--- test/demo/transitions.js
+++ test/demo/transitions.js
@@ -1,5 +1,8 @@
-const { State, Transitions } = require("../../src")
-const { BooleanTransition, SelfTransition, Transition } = Transitions
+const {
+    State,
+    Transitions: { BooleanTransition, SelfTransition, Transition },
+    util: { Router }
+} = require("../../src")
 
 const initialStateController = require("./initial-state/controllers.js")
 const browsingStateController = require("./browsing-state/controllers.js")
@@ -11,18 +14,20 @@ const browsingState = new State("BROWSING_STATE")
 const cartState = new State("CART_STATE")
 const orderState = new State("ORDER_STATE")
 
-initialState.addEvent("/login", new BooleanTransition(initialStateController.doLogin, browsingState, initialState))
+initialState.addEvent(Router.get("/login"), new BooleanTransition(initialStateController.doLogin, browsingState, initialState))
+initialState.addEvent(Router.get("/auth/:name/forgot-password/:actionId"), new BooleanTransition(initialStateController.doLogin, browsingState, initialState))
+initialState.addEvent(Router.get("/auth/:name"), new BooleanTransition(initialStateController.doLogin, browsingState, initialState))
 
-browsingState.addEvent("/view-more", new SelfTransition(browsingStateController.doViewMore))
-browsingState.addEvent("/logout", new Transition(browsingStateController.doLogout, initialState))
-browsingState.addEvent("/add-item-to-cart", new Transition(browsingStateController.doAddItem, cartState))
+browsingState.addEvent(Router.get("/view-more"), new SelfTransition(browsingStateController.doViewMore))
+browsingState.addEvent(Router.get("/logout"), new Transition(browsingStateController.doLogout, initialState))
+browsingState.addEvent(Router.get("/add-item-to-cart"), new Transition(browsingStateController.doAddItem, cartState))
 
-cartState.addEvent("/view-more", new Transition(browsingStateController.doViewMore, browsingState))
-cartState.addEvent("/clear-cart", new Transition(cartStateController.doClearCart, browsingState))
-cartState.addEvent("/checkout", new Transition(cartStateController.doCheckout, orderState))
+cartState.addEvent(Router.get("/view-more"), new Transition(browsingStateController.doViewMore, browsingState))
+cartState.addEvent(Router.get("/clear-cart"), new Transition(cartStateController.doClearCart, browsingState))
+cartState.addEvent(Router.get("/checkout"), new Transition(cartStateController.doCheckout, orderState))
 
-orderState.addEvent("/view-cart", new Transition(orderStateController.doViewCart, cartState))
-orderState.addEvent("/cancel-order", new Transition(orderStateController.doCancelOrder, browsingState))
-orderState.addEvent("/make-payment", new Transition(orderStateController.doMakePayment, browsingState))
+orderState.addEvent(Router.get("/view-cart"), new Transition(orderStateController.doViewCart, cartState))
+orderState.addEvent(Router.get("/cancel-order"), new Transition(orderStateController.doCancelOrder, browsingState))
+orderState.addEvent(Router.get("/make-payment"), new Transition(orderStateController.doMakePayment, browsingState))
 
 module.exports = { initialState }
